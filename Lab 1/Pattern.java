ae71d11a09c9bef9ac8ee7524c30ef68fadfa506
chourasiyavibes <105034290+chourasiyavibes@users.noreply.github.com>
2022-06-01 21:30:57 +0530

    lab 1 complete

---
+++ Lab 1/Pattern.java
@@ -0,0 +1,19 @@
+public class Main
+{
+	public static void main(String[] args) {
+		for(int i=1;i<5;i++)
+		{
+		    for(int j=i;j<4;j++)
+		    {
+		        System.out.print(" ");
+		    }
+		    for(int k=0;k<i;k++)
+		    {
+		        System.out.print("* ");
+		    }
+		    System.out.println();
+		}
+		
+		
+	}
+}
